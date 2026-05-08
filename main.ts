a1746a73938591cf79c81978947c350dc5820689
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2026-05-08 16:04:08 +0200

    basic test of TS connection. Button A send 1 button b send 0

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    ESP8266_IoT.setData(
+    "RB3LMXXQNCXW2QPD",
+    1
+    )
+    ESP8266_IoT.uploadData()
+    basic.showNumber(1)
+    basic.pause(20000)
+})
+input.onButtonPressed(Button.B, function () {
+    ESP8266_IoT.setData(
+    "RB3LMXXQNCXW2QPD",
+    0
+    )
+    ESP8266_IoT.uploadData()
+    basic.showNumber(0)
+    basic.pause(20000)
+})
 basic.showString("MQTT")
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("XHsc40000", "ncc1701a")
@@ -7,5 +25,5 @@ if (ESP8266_IoT.wifiState(false)) {
     basic.showIcon(IconNames.Yes)
 }
 basic.forever(function () {
-	
+    ESP8266_IoT.connectThingSpeak()
 })
